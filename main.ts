6a06d6f59c314c9f0f267beeb308bcbae1fda947
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-02-20 13:25:57 -0500

    I created code to read temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matthew Carnaffan
+ * Created on: Feb 2026
+ * This program tells the temputure when a button pressed
 */
+let temperature: number = input.temperature()
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
-basic.showString('Hello, World!')
+input.onButtonPressed(Button.A, function() {
+basic.showString('The current temperature is ' + (temperature).toString() + 'C.')
+})
+
