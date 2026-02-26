855038e688071d67573fcf070c8a7d8b9f819cb8
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-02-25 22:34:59 -0500

    I moved the creation of a variable to save on ram

--- main.ts
+++ main.ts
@@ -4,11 +4,12 @@
  * Created on: Feb 2026
  * This program tells the temputure when a button pressed
 */
-let temperature: number = input.temperature()
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function() {
+    let temperature: number = input.temperature()
 basic.showString('The current temperature is ' + (temperature).toString() + 'C.')
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
